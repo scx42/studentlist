b71f0513072d912089ea2cf03256d672cfcfba4d
scx42 <sunchan@sheridancollege.ca>
2020-01-23 15:24:20 -0500

    adding parttime student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,14 @@
+package studentlist;
+
+/**
+ * This class +++Insert Description Here+++
+ * 
+ * @author Changxin Sun
+ */
+public class PartTimeStudent extends Student{
+    private int numCourses;
+    public PartTimeStudent(String name, int numCourses){
+    super(name);
+    this.numCourses = numCourses;
+    }
+}
