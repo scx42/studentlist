d9443dcd36703cf1431ec0917a2f2e91e6c5ec67
scx42 <sunchan@sheridancollege.ca>
2020-02-19 22:56:59 -0500

    Commit by sun branch

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -60,7 +60,7 @@ public class StudentList {
             studentList[i] = student; // put student in the array
         }
 //ghdfffsdfs
-//by master
+//by master on feb19
         System.out.println("Now we print them out");
 
         for (Student student : studentList) {
